a251e6c04849bbc56eacef9a45583968d8581955
Juan Pablo Romero Laverde <pablo.romero2840@gmail.com>
2020-03-04 23:01:02 -0500

    Avances en html

--- src/app/owners/owners-edit/owners-edit.component.ts
+++ src/app/owners/owners-edit/owners-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnersService } from '../../shared/owners/owners.service';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-owners-edit',
@@ -8,18 +10,22 @@ import { OwnersService } from '../../shared/owners/owners.service';
 })
 export class OwnersEditComponent implements OnInit {
 
+
   dni = "";
   OwnerForID: any = [];
+  owner: any = {};
   constructor(private owners: OwnersService) {
 
     this.dni = this.owners.OwnerEdit;
     this.owners.getOWNERID(this.dni).subscribe((data: any) => {
-      this.OwnerForID = data;
+      this.OwnerForID = data._embedded.owners;
       console.log(this.OwnerForID);
     });
   }
 
-
+editOwner(editOwner: NgForm){
+console.log(editOwner);
+}
 
   ngOnInit() {
   }
